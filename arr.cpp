7f3c640f4daa2e4170e401d79538eaca47eba88d
satyajit450 <satyajeetsahu53@gmail.com>
2026-02-06 23:19:38 +0530

    Modify

--- arr.cpp
+++ arr.cpp
@@ -38,7 +38,7 @@ int main() {
 
     int min = arr[0];
     // Finding minimum element
-    for(int i = 1; i < n; i++) {1
+    for(int i = 1; i < n; i++) {
         if(arr[i] < min) {
             min = arr[i];
         }
